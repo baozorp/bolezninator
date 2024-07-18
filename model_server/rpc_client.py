563a256692456fd90ab0a7e18a880e89064b1ae5
Mikhail Shekunow <baozorp@gmail.com>
2024-07-18 09:34:51 +0300

    Fixed some bugs

--- model_server/rpc_client.py
+++ model_server/rpc_client.py
@@ -13,7 +13,7 @@ class RPCClient:
         while True:
             try:
                 self.connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host='localhost', port="5672"))
+                    pika.ConnectionParameters(host='rabbitmq', port="5672"))
 
                 self.channel = self.connection.channel()
                 self.channel.basic_consume(queue=self.queue_name,
@@ -26,13 +26,13 @@ class RPCClient:
     def _on_message(self, ch, method, props, body):
         ch.basic_ack(delivery_tag=method.delivery_tag)
         image_name = str(body)[2:-1]
-        url = f"http://localhost:8080/images/download_for_ML?image_name={
+        url = f"http://rest_api:8080/images/download_for_ML?image_name={
             image_name}"
         response = requests.get(url)
 
         if response.status_code == 200:
             print("Было")
-        url = f"http://localhost:8080/images/upload_from_ML?image_name={
+        url = f"http://rest_api:8080/images/upload_from_ML?image_name={
             image_name}"
         print(requests.post(url, files={"file": response.content}).status_code)
 
